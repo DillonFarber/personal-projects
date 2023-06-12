5a517e9a856012453a3647e7ee296ece72d4783d
dillonfarber <dillon.farber@gmail.com>
2023-06-12 11:29:30 -0500

    updating recycler view

---
+++ TheListofFavorites/app/src/main/java/com/github/dillonfarber/thelistoffavorites/MainListView.kt
@@ -0,0 +1,11 @@
+package com.github.dillonfarber.thelistoffavorites
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainListView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_list_view)
+    }
+}
