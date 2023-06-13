1cc60a5e0b4563a87e7d5b946ca1f5e33a1ae643
Dillon Farber <dillon.farber@gmail.com>
2023-06-12 20:07:11 -0500

    Adding API functions

--- TheListofFavorites/app/src/main/java/com/github/dillonfarber/thelistoffavorites/GamingAPI.kt
+++ TheListofFavorites/app/src/main/java/com/github/dillonfarber/thelistoffavorites/GamingAPI.kt
@@ -1,4 +1,7 @@
 package com.github.dillonfarber.thelistoffavorites
 
 class GamingAPI {
+
+    private val clientid = "wdcga06wfwk5r6ioi0el9i89sstg6m"
+    private val clientsec = "ga6xcepjh9tuucl4kjkwfof9he0bx6"
 }
